9f8d9da24dcf94c2470a0b7d571b9e0b6c000eaf
Katerou22 <xkaterou22@gmail.com>
2024-11-21 16:25:56 +0330

    Send logs to backend.

--- packages/logger.js
+++ packages/logger.js
@@ -1,20 +1,10 @@
-import axios from "axios";
+import axiosInstance from "./axios.js";
 
 
 export const log = (...args) => {
     console.log('\x1b[2m%s\x1b[0m', new Date().toISOString(), args)
 
-    const data = {
-        dt: new Date().toISOString().replace('T', ' ').replace('Z', ' UTC'),
-        message: args,
-    };
-
-    axios.post('https://in.logs.betterstack.com', data, {
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer CVefG9h9gDf4JwSfSp1J6JxX',
-        },
-    })
+    axiosInstance.post('/logger', args)
 
 
 }
